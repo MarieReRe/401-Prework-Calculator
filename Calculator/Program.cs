c918a66b3867a7ed3b9b8ffeda756ef941f88a54
Marie Antons <chefantons@gmail.com>
2020-05-03 16:19:37 -0500

    testing

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to the calculator app!");
+            calculatorApp();
+        }
+        public static void calculatorApp()
+        {
+
+        }
+    }
+}
